24e22ba5deeb8b939a129177e41ed53809d92da4
wandvvs <idc@mail.ru>
2024-01-26 20:29:00 +0300

    initial

---
+++ tests.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "vector.hpp"
+
+#include <cassert>
+#include <iostream>
+
+inline void push_back_test()
+{
+    vector<int> vec;
+    vec.push_back(1);
+    assert(vec[0] == 1);
+    std::cout << "[+] push_back_test" << std::endl;
+}
+
+inline void copy_ctor_test()
+{
+    vector<int> vec;
+    vec.push_back(1);
+    
+    vector<int> vec2(vec);
+    assert(vec2[0] == 1);
+    std::cout << "[+] copy_ctor_test" << std::endl;
+}
+
+inline void operator_equal_test()
+{
+    vector<int> vec;
+    vector<int> vec2;
+
+    vec.push_back(1);
+
+    vec2 = vec;
+    assert(vec2[0] == 1);
+    std::cout << "[+] operator_equal test" << std::endl;
+}
+
+inline void move_ctor_test()
+{
+    vector<int> vec;
+    vec.push_back(1);
+    vector<int> vec2(std::move(vec));
+    assert(vec2[0] == 1);
+    assert(vec.get_sz() == 0);
+    assert(vec.get_cap() == 0);
+    assert(vec.get_arr() == nullptr);
+
+    std::cout << "[+] move_ctor_test" << std::endl;
+}
+
+inline void operator_equal_move_test()
+{
+    vector<int> vec;
+    vector<int> vec2;
+    vec.push_back(1);
+    vec2 = std::move(vec);
+
+    assert(vec2[0] == 1);
+    assert(vec.get_sz() == 0);
+    assert(vec.get_cap() == 0);
+    assert(vec.get_arr() == nullptr);
+
+    std::cout << "[+] operator_equal_move_test" << std::endl;
+
+}
+
+inline void emplace_back_test()
+{
+    struct Test
+    {
+        std::string a,b,c;
+        Test(std::string A, std::string B, std::string C) : a(A), b(B), c(C) {}
+    };
+
+    vector<Test> vec;
+    std::string str = "Hello Test";
+
+    vec.emplace_back(str, "123", "321");
+    assert(!str.empty());
+    assert(vec[0].a == "Hello Test");
+    assert(vec[0].c == "321");
+    
+    std::cout << "[+] emplace_back_test" << std::endl;
+}
+
+inline void tests()
+{
+    push_back_test();
+    copy_ctor_test();
+    operator_equal_test();
+    move_ctor_test();
+    operator_equal_move_test();
+    emplace_back_test();
+}
---
+++ vector.hpp
@@ -0,0 +1,138 @@
+#pragma once
+
+#include <utility>
+#include <memory>
+
+template <typename T, typename Alloc = std::allocator<T>>
+class vector
+{
+private:
+    T* arr;
+    size_t sz;
+    size_t cap;
+    Alloc alloc;
+
+public:
+    using Traits = std::allocator_traits<Alloc>;
+
+    vector() : arr(nullptr), sz(0), cap(0) {}
+
+    ~vector()
+    {
+        for(size_t i = 0; i < sz; ++i)
+        {
+            Traits::destroy(alloc, arr+i);
+        }
+        Traits::deallocate(alloc, arr, cap);
+    }
+
+    vector(const vector& oth) : sz(oth.sz), cap(oth.cap)
+    {
+        arr = Traits::allocate(alloc, sz);
+        for(size_t i = 0; i < sz; ++i)
+        {
+            Traits::construct(alloc, arr+i, oth.arr[i]);
+        }
+    }
+
+    vector& operator=(const vector& oth)
+    {
+        if(this != &oth)
+        {
+            for(size_t i = 0; i < sz; ++i)
+            {
+                Traits::destroy(alloc, arr+i);
+            }
+            Traits::deallocate(alloc, arr, cap);
+
+            sz = oth.sz;
+            cap = oth.cap;
+            arr = Traits::allocate(alloc, oth.sz);
+
+            for(size_t i = 0; i < sz; ++i)
+            {
+                Traits::construct(alloc, arr+i, oth.arr[i]);
+            }
+        }
+        return *this;
+    }
+
+    vector(vector&& oth) noexcept
+    {
+        arr = std::exchange(oth.arr, nullptr);
+        sz = std::exchange(oth.sz, 0);
+        cap = std::exchange(oth.cap, 0);
+    }
+
+    vector& operator=(vector&& oth) noexcept
+    {
+        if(this != &oth)
+        {
+            for(size_t i = 0; i < sz; ++i)
+            {
+                Traits::destroy(alloc, arr+i);
+            }
+            Traits::deallocate(alloc, arr, cap);
+
+            arr = std::exchange(oth.arr, nullptr);
+            sz = std::exchange(oth.sz, 0);
+            cap = std::exchange(oth.cap, 0);
+        }
+        return *this;
+    }
+
+    void reserve(const size_t& new_cap)
+    {
+        if(new_cap <= cap) return;
+
+        T* newarr = Traits::allocate(alloc, new_cap);
+
+        for(size_t i = 0; i < sz; ++i)
+        {
+            Traits::construct(alloc, newarr + i, std::move_if_noexcept(arr[i]));
+        }
+
+        for(size_t i = 0; i < sz; ++i)
+        {
+            Traits::destroy(alloc, arr+i);
+        }
+        Traits::deallocate(alloc, arr, cap);
+
+        arr = newarr;
+        cap = new_cap;
+    }
+
+    void push_back(const T& value)
+    {
+        if(sz == cap)
+        {
+            reserve((cap == 0) ? 1 : cap*2);
+        }
+        Traits::construct(alloc, arr+sz, value);
+        sz++;
+    }
+
+    T& operator[](size_t index)
+    {
+        return arr[index];
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args)
+    {
+        if(sz == cap)
+        {
+            reserve(5);
+        }
+        Traits::construct(alloc, arr+sz, std::forward<Args>(args)...);
+        sz++;
+    } 
+
+    T* begin() {return arr;}
+    T* end() {return arr+sz;}
+
+    size_t get_sz() {return sz;}
+    size_t get_cap() {return cap;}
+    T* get_arr() {return arr;}
+};
+
